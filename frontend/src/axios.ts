18ab44da41055cfce2a17d18a6206cc5ddb1e5ec
Tomek <tomaszrajda@gmail.com>
2025-10-24 16:30:31 +0200

    axios change, vite api base url

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,11 +1,16 @@
 import axios from 'axios'
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+if (!BASE_URL) {
+    console.error("VITE_API_BASE_URL is not set. Check your .env files.");
+}
 // base url for all API requests
 const apiClient = axios.create({
-  baseURL: 'https://bckndinvestporfolioapp-production.up.railway.app/api',
+  baseURL: BASE_URL || '/api', 
   headers: {
     'Content-Type': 'application/json',
   },
-})
+});
 
 export default apiClient;
